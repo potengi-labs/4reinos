17fce5e4a619b316a127a572bd5798557b4bcb05
Potengi Labs <ladulcelara@gmail.com>
2026-06-25 16:23:41 -0300

    Add scene manager with scene definitions and loader
    Defines scenes with backgrounds and titles, and implements a function to load a scene based on its ID.

---
+++ assets/js/scene-manager.js
@@ -0,0 +1,82 @@
+const scenes = {
+
+
+mundo:{
+
+background:
+"assets/img/mundos/4reinos-2.png",
+
+titulo:
+"Aventura dos 4 Reinos"
+
+},
+
+
+
+COM_M001_E01:{
+
+background:
+"assets/img/missoes/com_m001/bg_caminho-praca1.png",
+
+titulo:
+"Praça da Expressão"
+
+},
+
+
+
+COM_M001_E02:{
+
+background:
+"assets/img/missoes/com_m001/bg_caminho-teatro1.png",
+
+titulo:
+"Teatro das Histórias"
+
+}
+
+
+};
+
+
+
+
+
+function loadScene(sceneID){
+
+
+const world =
+document.querySelector(".world");
+
+
+
+const scene =
+scenes[sceneID];
+
+
+
+if(!scene){
+
+console.error(
+"Cena não encontrada:",
+sceneID
+);
+
+return;
+
+}
+
+
+
+world.style.backgroundImage =
+`url(${scene.background})`;
+
+
+
+console.log(
+"Entrando na cena:",
+scene.titulo
+);
+
+
+}
