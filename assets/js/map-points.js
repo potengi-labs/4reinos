d639e14c47b4f159c2069158f02fcd097b5f29d8
Potengi Labs <ladulcelara@gmail.com>
2026-06-25 17:32:26 -0300

    Create map-points.js

---
+++ assets/js/map-points.js
@@ -0,0 +1,124 @@
+const mapPoints = [
+
+
+{
+id:"COM_M001_E01",
+
+nome:"Praça da Expressão",
+
+x:"22%",
+y:"82%",
+
+icone:"💬",
+
+acao:()=>{
+
+loadScene("COM_M001_E01");
+
+}
+
+},
+
+
+
+{
+id:"COM_M001_E02",
+
+nome:"Teatro das Histórias",
+
+x:"34%",
+y:"70%",
+
+icone:"🎭",
+
+acao:()=>{
+
+loadScene("COM_M001_E02");
+
+}
+
+},
+
+
+
+{
+id:"BIO_M002",
+
+nome:"Templo do Equilíbrio",
+
+x:"70%",
+y:"90%",
+
+icone:"🏛️",
+
+acao:()=>{
+
+loadScene("BIO_M002");
+
+}
+
+}
+
+
+];
+
+
+
+
+
+function createMapPoints(){
+
+
+const layer =
+document.createElement("div");
+
+
+layer.id="poi-layer";
+
+
+document
+.querySelector(".world")
+.appendChild(layer);
+
+
+
+mapPoints.forEach(point=>{
+
+
+const link =
+document.createElement("button");
+
+
+
+link.className="map-point";
+
+link.style.left =
+point.x;
+
+
+link.style.top =
+point.y;
+
+
+
+link.innerHTML =
+`
+<span>${point.icone}</span>
+<label>${point.nome}</label>
+`;
+
+
+
+link.onclick =
+point.acao;
+
+
+
+layer.appendChild(link);
+
+
+
+});
+
+
+}
