d3582fd6fb924d7708cc5ec437ae530f9e49e08a
Potengi Labs <ladulcelara@gmail.com>
2026-06-26 01:30:45 -0300

    Update missions.js

--- assets/js/missions.js
+++ assets/js/missions.js
@@ -1,6 +1,6 @@
 async function carregarMissao(id) {
   try {
-    const resposta = await fetch(`assets/missoes/data/json/${id}.json`);
+    const resposta = await fetch(`../missoes/data/json/${id}.json`);
 
     if (!resposta.ok) {
       throw new Error("Missão não encontrada: " + id);
