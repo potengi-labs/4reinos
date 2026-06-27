4561f4864712ae38f3c4f230ffcb3e67767dcb29
Potengi Labs <ladulcelara@gmail.com>
2026-06-27 15:43:05 -0300

    Refactor scene-manager.js to remove whitespace

--- assets/js/scene-manager.js
+++ assets/js/scene-manager.js
@@ -1,82 +1,46 @@
 const scenes = {
-
-
 mundo:{
-
 background:
 "assets/img/mundos/bg_4reinos0.png",
-
 titulo:
 "Aventura dos 4 Reinos"
-
 },
-
-
 
 COM_M001_E01:{
-
 background:
 "assets/img/missoes/com_m001/bg_caminho-praca1.png",
-
 titulo:
 "Praça da Expressão"
-
 },
 
-
-
 COM_M001_E02:{
-
 background:
 "assets/img/missoes/com_m001/bg_caminho-teatro1.png",
-
 titulo:
 "Teatro das Histórias"
-
 }
-
-
 };
 
-
-
-
-
 function loadScene(sceneID){
-
-
 const world =
 document.querySelector(".world");
-
-
-
 const scene =
 scenes[sceneID];
 
-
-
 if(!scene){
-
 console.error(
 "Cena não encontrada:",
 sceneID
 );
 
 return;
-
 }
-
-
 
 world.style.backgroundImage =
 `url(${scene.background})`;
-
-
 
 console.log(
 "Entrando na cena:",
 scene.titulo
 );
-
-
 }
