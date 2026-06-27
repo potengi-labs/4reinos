a1887e6820efcec16d8ef7fbddae36cb4412bf2d
Potengi Labs <ladulcelara@gmail.com>
2026-06-27 15:18:57 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,17 @@
-document.addEventListener("DOMContentLoaded",async ()=>{
+import { carregarMissao } from "./missions.js";
 
+document.addEventListener("DOMContentLoaded", async () => {
 
-const worldLayer =
-document.getElementById("world-layer");
+    const worldLayer = document.getElementById("world-layer");
+    const uiLayer = document.getElementById("ui-layer");
 
+    await loadComponent("../assets/components/world.html", worldLayer);
+    await loadComponent("../assets/components/ui.html", uiLayer);
 
-const uiLayer =
-document.getElementById("ui-layer");
+    loadScene("mundo");
 
+    createMapPoints();
 
-
-await loadComponent(
-"assets/components/world.html",
-worldLayer
-);
-
-
-
-await loadComponent(
-"assets/components/ui.html",
-uiLayer
-);
-
-
-
-loadScene("mundo");
-
-
-createMapPoints();
-
-
+    carregarMissao("missao1");
 
 });
