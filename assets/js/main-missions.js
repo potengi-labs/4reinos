47958f37f5169ef466630c27f96c6a9c77ac9cbf
Potengi Labs <ladulcelara@gmail.com>
2026-06-27 23:21:07 -0300

    Update main-missions.js

--- assets/js/main-missions.js
+++ assets/js/main-missions.js
@@ -1,18 +1,10 @@
 document.addEventListener("DOMContentLoaded",async ()=>{
 
-const worldLayer =
-document.getElementById("world-layer");
-
 const footerLayer =
 document.getElementById("footer-layer");
 
 const missioncomm001Layer =
 document.getElementById("mission-com-m001-layer");
-
-await loadComponent(
-"../assets/components/world.html",
-worldLayer
-);
 
 await loadComponent(
 "../assets/components/footer.html",
@@ -23,15 +15,6 @@ await loadComponent(
 "mission-com-m001.html",
 missioncomm001Layer
 );
-
-loadScene("mundo");
-
-createMapPoints();
-
-  
-// Inicializa o sistema de missões  
-  iniciarMissoes();
-
 
   
 });
